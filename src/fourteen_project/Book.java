fd6d9d968cc8cc3a9c2c46b794298d360f404e69
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-27 19:18:42 +0300

    main method

---
+++ src/fourteen_project/Book.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fourteen_project;
+
+/**
+ *
+ * @author Boro_s99
+ */
+public class Book implements IPrintable{
+    
+    String name;
+    String author;
+    int year;
+    
+    Book()
+    {
+        this("none", "none", 0);
+    }
+    
+    Book(String name, String author, int year)
+    {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+    
+
+      
+    public void Print()
+    {
+        System.out.println(name +" "+ author +" "+ year);
+    }
+
+
+    
+           
+    
+}
